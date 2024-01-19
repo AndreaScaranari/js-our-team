c28266bcfcaa29b7f653c09390fb6d598f651fc4
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-19 17:32:33 +0100

    bonus_1

--- js/script.js
+++ js/script.js
@@ -42,27 +42,40 @@ for (let member of teamMembers) {
 // # Milestone 2
 const list = document.getElementById("team-list");
 
+// let membersAttributes = "";
+
+// for (let member of teamMembers) {
+//     const {name, role, picture} = member;
+//     membersAttributes += `<li>${name}, ${role}, ${picture} </li>`
+// }
+
+// list.innerHTML = membersAttributes;
+
+// ? oppure
+
+// let forInMembers = "";
+
+// for (let member of teamMembers) {
+//     forInMembers += `<li><ul>`
+    
+//     for (let key in member) {
+//         forInMembers += `<li>${member[key]}</li>`
+//     }
+    
+//     forInMembers += `</li></ul>`
+//     }
+
+// list.innerHTML = forInMembers;
+
+// # Bonus 1
 let membersAttributes = "";
 
 for (let member of teamMembers) {
     const {name, role, picture} = member;
-    membersAttributes += `<li>${name}, ${role}, ${picture} </li>`
+    membersAttributes += 
+    `<li>${name}</li>
+    <li>${role}</li> 
+    <li><img src="assets/imgs/${picture}" alt="${name}"></li>`
 }
 
 list.innerHTML = membersAttributes;
-
-// oppure
-
-let forInMembers = "";
-
-for (let member of teamMembers) {
-    forInMembers += `<li><ul>`
-    
-    for (let key in member) {
-        forInMembers += `<li>${member[key]}</li>`
-    }
-    
-    forInMembers += `</li></ul>`
-    }
-
-list.innerHTML = forInMembers;
